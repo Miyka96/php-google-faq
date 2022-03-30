1f0c91a13c0a12637b02b4ecacb743610e37f3b7
Miyka96 <micaelaseminara@outlook.it>
2022-03-30 16:12:32 +0200

    update

--- index.php
+++ index.php
@@ -1,6 +1,28 @@
 <?php
 
-
+$qa=
+// first QA 
+[   
+    [
+        'q' => `Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?`,
+        'a' => [
+                    `<p class="answer">
+                        La recente <a href="#">decisione della Corte di giustizia dell'Unione europea</a> ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.
+                    </p>`,
+                    "pippo", 
+                ]
+    ],
+    [
+        'q' => `Come state implementando la recente decisione della Corte di giustizia dell'Unione europea (CGUE) relativa al diritto all'oblio?`,
+        'a' => [
+                    `<p class="answer">
+                        La recente <a href="#">decisione della Corte di giustizia dell'Unione europea</a> ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.
+                    </p>`,
+                    'pippo',
+                ]
+    ],
+];
+var_dump($qa);
 
 ?>
 
@@ -26,12 +48,12 @@
 
     <div class="container-fluid">
         <div class="container">
-            <h2 class="question">
+            <!-- <h2 class="question">
             
             </h2>
             <p class="answer">
-            
-            </p>
+            La recente <a href="#">decisione della Corte di giustizia dell'Unione europea</a> ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi.
+            </p> -->
         </div>
     </div>
 </body>
